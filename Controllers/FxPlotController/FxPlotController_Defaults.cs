6db412d4c98e39575c9a150685a8f09e81db6433
GateDigger <140338882+GateDigger@users.noreply.github.com>
2023-11-04 13:54:10 +0100

    Change default plot quality
    So we don't overheat someone's CPU

--- Controllers/FxPlotController/FxPlotController_Defaults.cs
+++ Controllers/FxPlotController/FxPlotController_Defaults.cs
@@ -4,7 +4,7 @@ namespace GDPlotter
 {
     public partial class FxPlotController : BasePlotController
     {
-        const int PLOT_QUALITY_DFLT = 32768;
+        const int PLOT_QUALITY_DFLT = 8192;
 
         const bool RENDER_FX_DFLT = true;
     }
